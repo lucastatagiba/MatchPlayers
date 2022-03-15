1467f836edbd00a0ae3bf2df9ebf82cb2f740c9e
lucastatagiba <marlucetata@hotmail.com>
2022-03-15 09:47:06 -0300

    feat: adding global styles and react-toastify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { ToastContainer } from "react-toastify";
+import GlobalStyles from "./GlobalStyles";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ToastContainer />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;

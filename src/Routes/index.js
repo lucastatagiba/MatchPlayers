00e1ebcff9313ec0ff16569076e036d8ec9a3f1b
lucastatagiba <marlucetata@hotmail.com>
2022-03-15 10:15:21 -0300

    feat: structuring all routes

---
+++ src/Routes/index.js
@@ -0,0 +1,19 @@
+import { Route, Switch } from "react-router-dom";
+import FeedPage from "../Pages/FeedPage";
+import LandingPage from "../Pages/LandingPage";
+import ProfilePage from "../Pages/ProfilePage";
+import RegisterPage from "../Pages/RegisterPage";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path={"/"} component={LandingPage} />
+      <Route exact path={"/register"} component={RegisterPage} />
+      <Route exact path={"/feed"} component={FeedPage} />
+      <Route exact path={"/profile/:name"} component={ProfilePage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  );
+};
+
+export default Routes;

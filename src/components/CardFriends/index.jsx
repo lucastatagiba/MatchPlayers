d016d89bb53d40312839eebb706fda6e3af8c196
leogm360 <leogm360@gmail.com>
2022-03-15 20:06:42 -0300

    feat: card friends

--- src/components/CardFriends/index.jsx
+++ src/components/CardFriends/index.jsx
@@ -1,8 +1,17 @@
-const CardFriends = () => {
+import { RiMessage2Line } from "react-icons/ri";
+
+import * as styles from "./index";
+
+const CardFriends = ({ image, name }) => {
   return (
-    <>
-      <h1>oi</h1>
-    </>
+    <styles.Container>
+      <styles.Image src={image} alt={`Pessoa ${name}`} />
+
+      <styles.Name>{name}</styles.Name>
+
+      <RiMessage2Line />
+    </styles.Container>
   );
 };
+
 export default CardFriends;

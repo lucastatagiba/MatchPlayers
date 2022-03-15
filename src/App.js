db6347f69c7193c57de7dc42f5d2e4464e8f246a
lucastatagiba <marlucetata@hotmail.com>
2022-03-15 09:24:51 -0300

    feat: global styles added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ToastContainer } from "react-toastify";
+import GlobalStyles from "./globalStyles";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+      <GlobalStyles />;
+    </>
+  );
+}
+
+export default App;

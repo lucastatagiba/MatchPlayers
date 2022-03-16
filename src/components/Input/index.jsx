ec81228ef9e5f251bd4b9b734d5014ab873561a1
Andre <heraandre@gmail.com>
2022-03-16 11:06:51 -0300

    feat: population input and button

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,25 @@
-const Input = () => {
+import { Container } from "./style";
+
+const Input = ({
+  bgcolor,
+  width,
+  height,
+  color,
+  bdradius,
+  placeholder,
+  icon,
+}) => {
   return (
     <>
-      <h1>oi</h1>
+      <Container
+        bgcolor={bgcolor}
+        width={width}
+        color={color}
+        bdradius={bdradius}
+        placeholder={placeholder}
+        icon={icon}
+        height={height}
+      ></Container>
     </>
   );
 };

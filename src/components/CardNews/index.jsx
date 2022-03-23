a3517ab5b7648d4efa26bacef4b5faf934b3fdec
Rodrigo <rfirmos@gmail.com>
2022-03-23 17:03:45 -0300

    refactor: refactor cardnews

--- src/components/CardNews/index.jsx
+++ src/components/CardNews/index.jsx
@@ -1,20 +1,22 @@
-import { Link } from "react-router-dom";
-import { Container } from "./style.js";
+import { Container, Image, Text } from "./style.js";
 
 const CardNews = ({ title, desc, imgNews, urlNews }) => {
   return (
-    <Container href={urlNews}>
-      <div className="info--news">
-        <img
+    <Container className="news" href={urlNews}>
+      <Container className="cardnews">
+        <Image
+          className="cardnews__logo"
           src="https://st.depositphotos.com/1016545/4532/i/450/depositphotos_45321779-stock-photo-letters-in-fire-letter-n.jpg"
           alt="News"
         />
-        <h4>{title}</h4>
-      </div>
-      <div className="desc--news">
-        {imgNews && <img src={imgNews} alt="News" />}
-        <p>{desc}</p>
-      </div>
+        <Text className="cardnews__title">{title}</Text>
+      </Container>
+      <Container className="cardnews__content">
+        {imgNews && (
+          <Image className="cardnews__photo" src={imgNews} alt="News" />
+        )}
+        <Text className="cardnews__description">{desc}</Text>
+      </Container>
     </Container>
   );
 };
